4b0125156cf969b058b91487363999721a4664b4
Crowdbotics <team@crowdbotics.com>
2021-08-25 20:06:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wandering_sun_30109Identifier",
+  url: "https://wandering_sun_30109Identifier.botics.co",
+  api: "https://wandering_sun_30109Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
